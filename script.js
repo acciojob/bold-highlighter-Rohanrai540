35c6548c4aec2190df45576d76c1da10ea0ba066
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-01-02 09:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+ const strongTags = document.querySelectorAll('strong');
+  strongTags.forEach(tag => {
+    tag.style.color = 'rgb(0, 128, 0)';
+  });
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+   const strongTags = document.querySelectorAll('strong');
+  strongTags.forEach(tag => {
+    tag.style.color = 'rgb(0, 0, 0)';
+  });
     
 }
